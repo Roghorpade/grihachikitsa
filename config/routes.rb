d7113171eed6670ed51fa83f5e1d184e3c759e39
Rohit Ghorpade <roghorpade@gmail.com>
2015-07-13 20:36:36 +0530

    Added user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-   root "pages#home"
+  root "pages#home"
+  get "user_page" => "pages#user_page"
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
